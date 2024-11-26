fc974f43a201824ad45debe6ce6f5b7bffebd2bf
Antonio Zaccaria <antonio.zaccaria@student.bigrock.it>
2024-11-26 12:24:28 +0100

    _private

--- config/_private.py
+++
@@ -1,7 +0,0 @@
-config = {
-    "llm_model": "G:\\Il mio Drive\\ai\\models\\Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf",
-    "audio_model": "G:\\Il mio Drive\\ai\\piper\\models\\it_IT-paola-medium.onnx",
-    "audio_model_json": "G:\\Il mio Drive\\ai\\piper\\models\\it_IT-paola-medium.onnx.json",
-    "audio_output": "output.wav",
-    "piper_exe": "G:\\Il mio Drive\\ai\\piper\\piper"
-}
--- config/paths.py
+++ config/paths.py
@@ -1,17 +1,23 @@
-import config._private as private
+import os
 
-
-'''
-NOTE:
-ensure that the paths are correct and that the files exist
-config = {
+# Verifica se il file _private.py esiste
+if not os.path.exists('config/_private.py'):
+    
+    with open('config/_private.py', 'w') as f:
+        f.write('''config = {
     "llm_model": "path/to/llm_model.gguf",
     "audio_model": "path/to/audio_model.onnx",
     "audio_model_json": "path/to/audio_model.json",
     "audio_output": "path/to/output.wav",
     "piper_exe": "path/to/piper.exe"
 }
-'''
+
+print("*"*50)
+print("ATTENTION: ensure that the paths are correct and that the files exist in config/_private.py")
+print("*"*50)
+''')
+    
+import config._private as private
 
 llm_model = private.config["llm_model"]
 audio_model = private.config["audio_model"]
